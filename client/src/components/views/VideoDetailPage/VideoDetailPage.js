79540f11232c706d6ee3b413fbde5906bc419cdb
hellomac87 <hellomac87@gmail.com>
2020-02-19 22:27:03 +0900

    디테일 비디오 페이지에 Side 비디오 생성

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Row, Col, List, Avatar } from "antd";
 import Axios from "axios";
+import SideVideo from "./Sections/SideVideo";
 
 const VideoDetailPage = props => {
   const [videoDetail, setVideoDetail] = useState([]);
@@ -41,7 +42,7 @@ const VideoDetailPage = props => {
           </div>
         </Col>
         <Col lg={6} xs={24}>
-          Side Videos
+          <SideVideo />
         </Col>
       </Row>
     );
