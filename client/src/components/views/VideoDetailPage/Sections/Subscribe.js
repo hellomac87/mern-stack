fb83b27797e776a105f35276697c25db56329f3d
hellomac87 <hellomac87@gmail.com>
2020-02-24 23:48:17 +0900

    구독 기능

--- client/src/components/views/VideoDetailPage/Sections/Subscribe.js
+++ client/src/components/views/VideoDetailPage/Sections/Subscribe.js
@@ -4,6 +4,7 @@ import Axios from "axios";
 const Subscribe = ({ userTo }) => {
   const [subscribeNumber, setSubscribeNumber] = useState(0);
   const [subscribed, setSubscribed] = useState(false);
+
   useEffect(() => {
     let variable = {
       userTo
@@ -28,11 +29,41 @@ const Subscribe = ({ userTo }) => {
       }
     });
   }, []);
+
+  const onSubscribe = () => {
+    let subscribeVariable = {
+      userTo: userTo,
+      userFrom: localStorage.getItem("userId")
+    };
+
+    if (subscribed) {
+      // 아마 구독중이라면
+      Axios.post(`/api/subscribe/unSubscribe`, subscribeVariable).then(res => {
+        if (res.data.success) {
+          setSubscribeNumber(subscribeNumber - 1);
+          setSubscribed(!subscribed);
+        } else {
+          alert("구독 취소에 실패 했습니다.");
+        }
+      });
+    } else {
+      // 어작 구독중이 아니라면
+      Axios.post(`/api/subscribe/subscribe`, subscribeVariable).then(res => {
+        if (res.data.success) {
+          setSubscribeNumber(subscribeNumber + 1);
+          setSubscribed(!subscribed);
+        } else {
+          alert("구독에 실패 했습니다.");
+        }
+      });
+    }
+  };
+
   return (
     <div>
       <button
         style={{
-          backgroundColor: `${subscribed ? "#AAAAAA" : "#CC0000"}`,
+          backgroundColor: `${!subscribed ? "#AAAAAA" : "#CC0000"}`,
           borderRadius: "4px",
           color: "white",
           padding: "10px 16px",
@@ -40,7 +71,7 @@ const Subscribe = ({ userTo }) => {
           fontSize: "1rem",
           textTransform: "uppercase"
         }}
-        onClick
+        onClick={onSubscribe}
       >
         {subscribeNumber} {subscribed ? "Subscribed" : "Subscribe"}
       </button>
